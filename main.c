8428529ddd7a1bfa72ad84375fd46f0178613b75
Artem Titoulenko <artem.titoulenko@gmail.com>
2012-12-22 02:12:59 -0500

    moved the line reading portion into the safe  portion

--- main.c
+++ main.c
@@ -7,8 +7,7 @@
 int main (int argc, char * argv[]) {
   FILE * fd;
   struct Parser * parser;
-  char * word;
-  char * line;
+  char * word, * line;
 
   if (argc != 2) {
     fprintf(stderr, "usage: word_parser <file>\n");
@@ -19,20 +18,25 @@ int main (int argc, char * argv[]) {
   if (fd != NULL) {
     parser = parser_new(fd);
 
+
     while ( (word = parser_next_word(parser)) != NULL ) {
       printf("%s\n", word);
       free(word);
     }
-  }
 
-  rewind(fd);
+  	rewind(fd);
 
-  while ( (line = parser_next_line(parser)) != NULL && line[0] != 0 ) {
-    printf("line: [%s]\n", line);
-    free(line);
-  }
+		/* read each line, point `line` at the memory malloc'd in parser_next_line.
+		   `line` may be an empty line*/
+  	while ( (line = parser_next_line(parser)) != NULL) {
+    	printf("line: [%s]\n", line);
+    	free(line);
+  	}
 
-  parser_destroy(parser);
+  	parser_destroy(parser);
+	} else {
+		fprintf(stderr, "Unable to open %s\n", argv[1]);
+	}
 
   return 0;
 }
